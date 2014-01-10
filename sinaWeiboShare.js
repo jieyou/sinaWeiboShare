ebb988fcc6f7fd8cfa908444c9a5e424a4372b53
Jieyou Qin <qinjieyou@gmail.com>
2014-01-10 16:58:11 +0800

    原本用title字段表示被分享的内容，现在也可以用text字段来表示
    原本用title字段表示被分享的内容，现在也可以用text字段来表示

--- sinaWeiboShare.js
+++ sinaWeiboShare.js
@@ -1,14 +1,20 @@
-// see https://github.com/jieyou/sinaWeiboShare
+/*! see https://github.com/jieyou/sinaWeiboShare
+*/
 ;(function(window){
 	var sinaUrlPrefix = 'http://service.weibo.com/share/'+(/android|webos|ip(hone|ad|od)|opera (mini|mobi|tablet)|iemobile|windows.+(phone|touch)|mobile|fennec|kindle (Fire)|Silk|maemo|blackberry|playbook|bb10\; (touch|kbd)|Symbian(OS)|Ubuntu Touch/i.test(navigator.userAgent)?'mobile':'share')+'.php?' // see https://github.com/pinceladasdaweb/isMobile/blob/master/ismobile.js
 	
 	// options
-	//  title String 分享内容，无需encodeURI
+	//  title或text String 分享内容，无需encodeURI
 	//  url String 分享链接，无需encodeURI
 	//  pic String 分享图片的url，无需encodeURI。新浪的接口多张图片尚未完全开放，暂时只能分享一张
 	//  ralateUid String 或 Number 相关微博Uid，如果有此项，分享内容会自动 @相关微博
 	function sinaWeiboShare(options){
 		var key,urlArray = []
+		// 我们调起客户端分享的接口用text代表分享的内容，而sina微博的web分享接口用title表示分享的内容。为了兼容和减少工作量，可以用text代表title参数
+		if(options.text){
+			options.title = options.text
+			delete options.text
+		}
 		if(options){
 			for(key in options){
 				if(options.hasOwnProperty(key)){
