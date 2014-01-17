0339b2daede526143a7fd4f87cac385e48a0c204
Jieyou Qin <qinjieyou@gmail.com>
2014-01-17 15:27:36 +0800

    v 1.2.0
    现在拆分出 @.getUrl@ 和 @.action@ 方法。
    加入appkey参数

--- sinaWeiboShare.js
+++ sinaWeiboShare.js
@@ -8,7 +8,8 @@
 	//  url String 分享链接，无需encodeURI
 	//  pic String 分享图片的url，无需encodeURI。新浪的接口多张图片尚未完全开放，暂时只能分享一张
 	//  ralateUid String 或 Number 相关微博Uid，如果有此项，分享内容会自动 @相关微博
-	function sinaWeiboShare(options){
+	// 	appkey String 或 Number 分享来源的appkey，如果有此项，会在微博正文地下，显示“来自XXX”
+	function getUrl(options){
 		var key,urlArray = []
 		// 我们调起客户端分享的接口用text代表分享的内容，而sina微博的web分享接口用title表示分享的内容。为了兼容和减少工作量，可以用text代表title参数
 		if(options.text){
@@ -25,19 +26,29 @@
 							urlArray.push(key+'='+encodeURIComponent(options[key]))
 							break;
 						case 'ralateUid':
+						case 'appkey':
 							urlArray.push(key+'='+options[key])
 							break;
 					}
 				}
 			}
 		}
-		try{
-			window.open(sinaUrlPrefix+urlArray.join('&'))
-		}catch(e){
-			
-		}
+		return sinaUrlPrefix+urlArray.join('&')
+		
 	}
 
-	window.sinaWeiboShare = sinaWeiboShare
+	function action(optionsOrUrl){
+		if(typeof(optionsOrUrl) != 'string'){
+			optionsOrUrl = getUrl(optionsOrUrl)
+		}
+		try{
+			window.open(optionsOrUrl)
+		}catch(e){}
+	}
+
+	window.sinaWeiboShare = {
+		action:action,
+		getUrl:getUrl
+	}
 	
 })(window)
