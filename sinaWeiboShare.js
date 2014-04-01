3e57f24defd89678267bf80acafc5fe2ac71794a
jieyou <qinjieyou@gmail.com>
2014-04-01 13:08:47 +0800

    改进标点和空格

--- sinaWeiboShare.js
+++ sinaWeiboShare.js
@@ -7,11 +7,11 @@
 	var sinaUrlPrefix = 'http://service.weibo.com/share/'+(/android|webos|ip(hone|ad|od)|opera (mini|mobi|tablet)|iemobile|windows.+(phone|touch)|mobile|fennec|kindle (Fire)|Silk|maemo|blackberry|playbook|bb10\; (touch|kbd)|Symbian(OS)|Ubuntu Touch/i.test(navigator.userAgent)?'mobile':'share')+'.php?' // see https://github.com/pinceladasdaweb/isMobile/blob/master/ismobile.js
 	
 	// options
-	//  title或text String 分享内容，无需encodeURI
-	//  url String 分享链接，无需encodeURI
-	//  pic String 分享图片的url，无需encodeURI。新浪的接口多张图片尚未完全开放，暂时只能分享一张
-	//  ralateUid String 或 Number 相关微博Uid，如果有此项，分享内容会自动 @相关微博
-	// 	appkey String 或 Number 分享来源的appkey，如果有此项，会在微博正文地下，显示“来自XXX”
+	//  title或text [类型String] 分享内容，无需encodeURI
+	//  url [类型String] 分享链接，无需encodeURI
+	//  pic [类型String] 分享图片的url，无需encodeURI。新浪的接口多张图片尚未完全开放，暂时只能分享一张
+	//  ralateUid [类型String 或 Number] 相关微博Uid，如果有此项，分享内容会自动 @相关微博
+	//  appkey [类型String 或 Number] 分享来源的appkey，如果有此项，会在微博正文地下，显示“来自XXX”
 	function getUrl(options){
 		var key,urlArray = []
 		// 我们调起客户端分享的接口用text代表分享的内容，而sina微博的web分享接口用title表示分享的内容。为了兼容和减少工作量，可以用text代表title参数
